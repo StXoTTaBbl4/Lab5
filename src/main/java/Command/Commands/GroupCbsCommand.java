f14c5899f12775fc0b24d1f4ea364e0f81fb9e59
Александр <arumskiy@gmail.com>
2022-03-23 08:20:35 +0300

    5 лаба

--- src/main/java/Command/Commands/GroupCbsCommand.java
+++ src/main/java/Command/Commands/GroupCbsCommand.java
@@ -1,6 +1,25 @@
 package Command.Commands;
 
-public class GroupCbsCommand {
+import Command.ICommand;
+import DataClasses.Worker;
+
+import java.util.LinkedList;
+
+public class GroupCbsCommand implements ICommand {
+    @Override
+    public LinkedList<Worker> handle(String args, LinkedList<Worker> WorkersData) {
+        return WorkersData;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
     //group counting by salary
 
 }
